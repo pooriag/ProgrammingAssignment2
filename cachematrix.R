c0eec4e0cb3d3ec12a8f1a208e907c4879cc6939
pog <pooriagolestanipg@gmail.com>
2024-07-04 15:58:52 +0330

    completed the r script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creating matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <<- NULL
+      set <- function(m) x <<- m
+      get <- function() x
+      
+      set_inv <- function(i) inv <<- i
+      get_inv <- function() inv
+      
+      list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+## calculating invers of matrix and caching it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$get_inv()
+        if(!is.null(inv)) return(inv)
+        
+        inv <- solve(x$get() ,...)
+        x$set_inv(inv)
+        
+        inv
 }
